39e785fca9c9570e5d9e63e85277ed19636ebe23
Ruan Vitor Scherer <ruan.scherer@outlook.com>
2021-01-25 12:08:15 -0300

    feat: add poppins font

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -20,6 +20,8 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <title>IFood - Delivery de Comida</title>
         <meta charSet="utf-8" />
         <link rel="shortcut icon" type="image/svg" href="/favicon.svg"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
       </Head>
       <Header />
       <div className={router.pathname === "/" ? 'pt-40' : 'pt-28'}></div>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: '#F3F3F3',
+        dark: '#1e1849',
+        semidark: '#3f3b66',
+        legend: '#554ab2',
+        placeholder: '#afafaf',
+        error: '#ad0a0a',
+        primary: '#f87652',
+        secondary: '#554ab2',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
